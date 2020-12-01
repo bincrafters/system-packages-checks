a5d8d212283a7b8ddc54ba7f5d6ee7b51e24e78d
Eric Lemanissier <eric.lemanissier@gmail.com>
2020-12-01 19:07:22 +0100

    remove install command from matrix

--- main.py
+++ main.py
@@ -80,30 +80,29 @@ class MatrixGenerator:
                 config = yaml.safe_load(file)
                 if "system" not in config["versions"]:
                     return
-            install_python = {
-                "opensuse/tumbleweed": "zypper --non-interactive install python3 python3-pip python3-setuptools python3-wheel tar gzip gcc-c++ make",
-                "opensuse/leap":       "zypper --non-interactive install python3 python3-pip python3-setuptools python3-wheel tar gzip gcc-c++ make",
-                "debian:10": "apt-get update && apt-get -qq install -y --no-install-recommends python3-pip python3-setuptools pkg-config g++ make",
-                "debian:9":  "apt-get update && apt-get -qq install -y --no-install-recommends python3-pip python3-setuptools pkg-config g++ make",
-                "ubuntu:hirsute": "apt-get update && apt-get -qq install -y --no-install-recommends python3-pip python3-setuptools pkg-config g++ make",
-                "ubuntu:groovy":  "apt-get update && apt-get -qq install -y --no-install-recommends python3-pip python3-setuptools pkg-config g++ make",
-                "ubuntu:focal":   "apt-get update && apt-get -qq install -y --no-install-recommends python3-pip python3-setuptools pkg-config g++ make",
-                "ubuntu:bionic":  "apt-get update && apt-get -qq install -y --no-install-recommends python3-pip python3-setuptools pkg-config g++ make",
-                "ubuntu:xenial":  "apt-get update && apt-get -qq install -y --no-install-recommends python3-pip python3-setuptools pkg-config g++ make",
-                "centos:8":  "yum -y install dnf-plugins-core && yum config-manager --set-enabled PowerTools && yum install -y python3 python3-wheel python3-pip pkgconf-pkg-config gcc-c++ make",
-                "archlinux": "pacman -Syu --noconfirm python-pip gcc pkgconf gcc make",
-                "fedora:33": "dnf -y install python3 python3-wheel python3-pip pkgconf-pkg-config gcc-c++ make",
-                "fedora:32": "dnf -y install python3 python3-wheel python3-pip pkgconf-pkg-config gcc-c++ make",
-                "fedora:31": "dnf -y install python3 python3-wheel python3-pip pkgconf-pkg-config gcc-c++ make",
+            distros = {
+                "opensuse/tumbleweed",
+                "opensuse/leap",
+                "debian:10",
+                "debian:9",
+                "ubuntu:hirsute",
+                "ubuntu:groovy",
+                "ubuntu:focal",
+                "ubuntu:bionic",
+                "ubuntu:xenial",
+                "centos:8",
+                "archlinux",
+                "fedora:33",
+                "fedora:32",
+                "fedora:31",
             }
-            for container in install_python:
+            for distro in distros:
                 res.append({
                     'package': package,
-                    'distro': container,
+                    'distro': distro,
                     'repo': repo,
                     'ref': ref,
                     'folder': config["versions"]["system"]["folder"],
-                    'install-python': install_python[container],
                     'pr': pr,
                 })
                 
